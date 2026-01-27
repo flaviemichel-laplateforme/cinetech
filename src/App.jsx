783e53471842eb039b907a3a27e88ab76ff558a8
flaviemichel-laplateforme <flavie.michel@laplateforme.io>
2026-01-27 10:36:02 +0100

    Ajout des routes pour les pages Home, Detail et Favoris ; suppression du composant Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Detail from "./pages/Detail/Detail";
+import Favoris from "./pages/Favoris/Favoris";
 
 function App() {
     return (
@@ -6,13 +9,13 @@ function App() {
             {/* Le header sera placé ici après */}
             <Routes>
                 {/* Route Accueil */}
-                <Route> path="/" element={<Home />}</Route>
+                <Route path="/" element={<Home />} />
 
                 {/* Route de detail par id */}
-                <Route> path="/detail/:id" element={<Detail />}</Route>
+                <Route path="/detail/:id" element={<Detail />} />
 
                 {/* Route Favoris */}
-                <Route> path="/favoris" element={<Favoris />}</Route>
+                <Route path="/favoris" element={<Favoris />} />
 
                 {/*Ajouter /films et /series plus tard */}
 
--- src/pages/Home/Home.jsx
+++
@@ -1,5 +0,0 @@
-function Home() {
-    return <h1>Page d'accueil</h1>
-}
-
-export default Home;
